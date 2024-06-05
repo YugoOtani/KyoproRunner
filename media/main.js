23c2e74fec5ea7fc337fa262f865c1e99d4052bb
YugoOtani <107762166+YugoOtani@users.noreply.github.com>
2024-06-05 16:48:41 +0900

    add pretttier

--- media/main.js
+++ media/main.js
@@ -1,10 +1,7 @@
-
 (function () {
-    const vscode = acquireVsCodeApi();
-    document.querySelector('.execute-button').addEventListener('click', () => {
-        const commandInput = document.querySelector('#command-input').value;
-        vscode.postMessage({ type: 'executeCommand', value: commandInput });
-
-    });
-
-}());
+  const vscode = acquireVsCodeApi();
+  document.querySelector(".execute-button").addEventListener("click", () => {
+    const commandInput = document.querySelector("#command-input").value;
+    vscode.postMessage({ type: "executeCommand", value: commandInput });
+  });
+})();
--- src/extension.ts
+++ src/extension.ts
@@ -1,96 +1,83 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 const EXTENSION_NAME = "kyopro executer";
 
 export function activate(context: vscode.ExtensionContext) {
+  const provider = new ExecuteViewProvider(context.extensionUri);
 
-	const provider = new ExecuteViewProvider(context.extensionUri);
-
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(ExecuteViewProvider.viewType, provider)
-	);
-
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      ExecuteViewProvider.viewType,
+      provider,
+    ),
+  );
 }
 
 const openTerminal = () => {
-	const workspaceFolders = vscode.workspace.workspaceFolders;
-    if (!workspaceFolders) {
-        vscode.window.showErrorMessage('No workspace folder open');
-        return null;
-    }
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (!workspaceFolders) {
+    vscode.window.showErrorMessage("No workspace folder open");
+    return null;
+  }
 
-    const workspacePath = workspaceFolders[0].uri.fsPath;
-	let terminal = vscode.window.terminals.find(
-		term=> term.name === EXTENSION_NAME
-		&& !term.state.isInteractedWith);
-	if (!terminal){
-		terminal = vscode.window.createTerminal({
-				name: EXTENSION_NAME,
-				cwd: workspacePath
-		});
-	}
-	return terminal;
+  const workspacePath = workspaceFolders[0].uri.fsPath;
+  let terminal = vscode.window.terminals.find(
+    (term) => term.name === EXTENSION_NAME && !term.state.isInteractedWith,
+  );
+  if (!terminal) {
+    terminal = vscode.window.createTerminal({
+      name: EXTENSION_NAME,
+      cwd: workspacePath,
+    });
+  }
+  return terminal;
 };
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
 
 class ExecuteViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "rust-worksheet.executeCommand";
 
-	public static readonly viewType = 'rust-worksheet.executeCommand';
+  constructor(private readonly _extensionUri: vscode.Uri) {}
 
-	private _view?: vscode.WebviewView;
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    _context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken,
+  ) {
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    };
 
-	constructor(
-		private readonly _extensionUri: vscode.Uri,
-	) { }
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-	public resolveWebviewView(
-		webviewView: vscode.WebviewView,
-		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken,
-	) {
-		this._view = webviewView;
+    webviewView.webview.onDidReceiveMessage(async (data) => {
+      switch (data.type) {
+        case "executeCommand": {
+          this._executeCommand(data.value);
+          break;
+        }
+      }
+    });
+  }
+  private _executeCommand(input: string) {
+    const term = openTerminal();
+    term?.show();
+    term?.sendText(input);
+  }
 
-		webviewView.webview.options = {
-			// Allow scripts in the webview
-			enableScripts: true,
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js"),
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css"),
+    );
 
-			localResourceRoots: [
-				this._extensionUri
-			]
-		};
-		
-		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
 
-		webviewView.webview.onDidReceiveMessage(async data => {
-			switch (data.type) {
-				case 'executeCommand':
-				{
-					this._executeCommand(data.value);
-					break;
-				}
-			}
-		});
-	}
-	private _executeCommand(input:string){
-		const term = openTerminal();
-		term?.show();
-		term?.sendText(input);
-	}
-
-	private _getHtmlForWebview(webview: vscode.Webview) {
-		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
-		// Do the same for the stylesheet.
-		//const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
-		//const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
-		//
-		
-		// Use a nonce to only allow a specific script to be run.
-		const nonce = getNonce();
-
-		return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
 		<html lang="en">
 		<head>
 			<meta charset="UTF-8">
@@ -107,14 +94,15 @@ class ExecuteViewProvider implements vscode.WebviewViewProvider {
 			<script nonce="${nonce}" src="${scriptUri}"></script>
 		</body>
 		</html>`;
-	}
+  }
 }
 
 function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
