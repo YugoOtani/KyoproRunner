f215a18829723dfa5dcf848e0d2c9006bba1586c
YugoOtani <107762166+YugoOtani@users.noreply.github.com>
2024-06-05 16:36:08 +0900

    initial commit

---
+++ media/main.js
@@ -0,0 +1,10 @@
+
+(function () {
+    const vscode = acquireVsCodeApi();
+    document.querySelector('.execute-button').addEventListener('click', () => {
+        const commandInput = document.querySelector('#command-input').value;
+        vscode.postMessage({ type: 'executeCommand', value: commandInput });
+
+    });
+
+}());
---
+++ src/extension.ts
@@ -0,0 +1,120 @@
+import * as vscode from 'vscode';
+const EXTENSION_NAME = "kyopro executer";
+
+export function activate(context: vscode.ExtensionContext) {
+
+	const provider = new ExecuteViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(ExecuteViewProvider.viewType, provider)
+	);
+
+}
+
+const openTerminal = () => {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+        vscode.window.showErrorMessage('No workspace folder open');
+        return null;
+    }
+
+    const workspacePath = workspaceFolders[0].uri.fsPath;
+	let terminal = vscode.window.terminals.find(
+		term=> term.name === EXTENSION_NAME
+		&& !term.state.isInteractedWith);
+	if (!terminal){
+		terminal = vscode.window.createTerminal({
+				name: EXTENSION_NAME,
+				cwd: workspacePath
+		});
+	}
+	return terminal;
+};
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
+class ExecuteViewProvider implements vscode.WebviewViewProvider {
+
+	public static readonly viewType = 'rust-worksheet.executeCommand';
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+
+			localResourceRoots: [
+				this._extensionUri
+			]
+		};
+		
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+		webviewView.webview.onDidReceiveMessage(async data => {
+			switch (data.type) {
+				case 'executeCommand':
+				{
+					this._executeCommand(data.value);
+					break;
+				}
+			}
+		});
+	}
+	private _executeCommand(input:string){
+		const term = openTerminal();
+		term?.show();
+		term?.sendText(input);
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+		// Do the same for the stylesheet.
+		//const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+		//const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+		//
+		
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link href="${styleMainUri}" rel="stylesheet">
+			<title>Input</title>
+		</head>
+		<body>
+			<textarea id="command-input"></textarea>
+
+			<button class="execute-button">Run</button>
+
+			<script nonce="${nonce}" src="${scriptUri}"></script>
+		</body>
+		</html>`;
+	}
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
